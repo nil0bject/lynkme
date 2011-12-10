83573eed9bfe06fd368f5f74800c40d214e74b71
nil0bject <nilobject@gmail.com>
2011-12-11 01:31:48 +1000

    [heroku push]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lynkme::Application.config.secret_token = 'c3fc265049db7eb953e0c527fc197f7e7bf29e8d965ee91c07f2ad92cc94b918826d8a36f7ac7f23c13373972c0c123cbe2f7fa02a6e66c9fbcfc6107064eda8'
