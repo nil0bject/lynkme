fc2437d6f0c8ec4653735996672218dafea3093f
nil0bject <nilobject@gmail.com>
2011-12-10 17:47:34 +1000

    [Rails Initialised]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lynkme::Application.config.secret_token = 'd255940a2fb5e748911013b20b364b456117454ac26a8a1119329d79abb8f4a73b23eec453e25c1645e0604c1848957af50dc1ed074e6eebb8eec8513ab98af6'
