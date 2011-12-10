d092988fa60a4fce5adcfacaa11ad47eb70f3185
nil0bject <nilobject@gmail.com>
2011-12-10 22:12:36 +1000

    [upgraded rails]

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Lynkme::Application.config.secret_token = 'd255940a2fb5e748911013b20b364b456117454ac26a8a1119329d79abb8f4a73b23eec453e25c1645e0604c1848957af50dc1ed074e6eebb8eec8513ab98af6'
+Lynkme::Application.config.secret_token = 'f54b415f20c84d827c3bfd4a181e9ad295e24762a7def296295fc66c7147d41b4970a8ee7b64404e05615d3fb6fda60c795a998c89f6068d7be6872e098ec2c5'
