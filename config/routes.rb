906df623ca423b189f9840ce496b3d48363ee0b4
nil0bject <nilobject@gmail.com>
2011-12-10 22:31:31 +1000

    [rails configured]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Lynkme::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  root :to => 'application#index'
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
