0b6b1998a7d21754e601cfdbb84ec53013df52bb
nil0bject <nilobject@gmail.com>
2011-12-11 01:19:27 +1000

    [cleaned migration]

--- db/migrate/20111210142604_create_users.rb
+++ db/migrate/20111210142604_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :email
       t.string :password
-      t.boolean :type
+      t.boolean :supplier
 
       t.timestamps
     end
--- db/migrate/20111210150327_rename_type_to_supplier.rb
+++
@@ -1,9 +0,0 @@
-class RenameTypeToSupplier < ActiveRecord::Migration
-  def up
-    remove_column(:users, :type)
-    add_column(:users, :supplier, :boolean)
-  end
-
-  def down
-  end
-end
