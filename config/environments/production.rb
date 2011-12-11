e737cdd3aadc763e15dda9314d7e7137daf07366
nil0bject <nilobject@gmail.com>
2011-12-11 11:34:34 +1000

    [#3] assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Lynkme::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.assets.precompile += %w( debug.css registration.js  )
+  # config.assets.precompile += %w( debug.css registration.js  )
 
   # Code is not reloaded between requests
   config.cache_classes = true
