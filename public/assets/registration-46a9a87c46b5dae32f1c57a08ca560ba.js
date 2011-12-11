a3422a544e19c12c0c19ef7a85407105907adda8
nil0bject <nilobject@gmail.com>
2011-12-11 10:06:22 +1000

    [#3] compile assets

---
+++ public/assets/registration-46a9a87c46b5dae32f1c57a08ca560ba.js
@@ -0,0 +1,1 @@
+$("input#password_confirmation").parent().hide(),$(".login h3.register").click(function(){$(".login input#password_confirmation").parent().fadeIn(),$(".login .actions input").val("Register")})
