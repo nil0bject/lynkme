d221ca49c7f1eeca4e477153252513b547214552
nil0bject <nilobject@gmail.com>
2011-12-11 10:00:58 +1000

    [#3] compile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Lynkme::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.assets.precompile += %w( registration.js  )
 
   # Code is not reloaded between requests
   config.cache_classes = true
@@ -9,7 +10,7 @@ Lynkme::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
