e43867513ae6f852df4594587ef3297fc908cd8c
nil0bject <nilobject@gmail.com>
2011-12-11 11:48:58 +1000

    [#3] assets

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,6 +1,6 @@
-$('input#password_confirmation').parent().hide()
-
-$('.login h3.register').click(function() {
-  $('.login input#password_confirmation').parent().fadeIn()
-  $('.login .actions input').val('Register')
+$(document).ready(function() {$('.login input#password_confirmation').parent().hide()
+  $('.login h3.register').click(function() {
+    $('.login input#password_confirmation').parent().fadeIn()
+    $('.login .actions input').val('Register')
+  });
 });
